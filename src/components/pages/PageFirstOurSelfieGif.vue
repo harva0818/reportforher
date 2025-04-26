d607742651d37472fee85e67cf035a40cda35c4f
Hartono <74315370+hartono0297@users.noreply.github.com>
2025-04-26 15:45:48 +0700

    test3

--- src/components/pages/PageFirstOurSelfieGif.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="w-full h-full flex items-center justify-center bg-gradient-to-br from-purple-100 via-pink-50 to-rose-100"
-  >
-    <div class="backdrop-blur-md p-6 rounded-2xl text-center max-w-md space-y-4 mt-20">
-      <img src="/pages/ourselfie.jpg" alt="Love illustration" class="w-[500px] object-contain" />
-    </div>
-  </div>
-</template>
